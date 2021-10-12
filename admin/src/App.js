3abacc8bbf60f0ec43e5618b24df98d6dcc91864
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2021-10-12 04:31:05 +0000

    Initial commit

---
+++ admin/src/App.js
@@ -0,0 +1,11 @@
+import Topbar from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div>
+    <Topbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <div className="topbarWrapper">
+      <div className="topLeft">left</div>
+      <div className="topRight">Right</div>
+    </div>
+  )
+}
